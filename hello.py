420af73aaa9bbf58c3c48edcb2b1e3246eb6a4ae
Camilla Gustafsson <cagus@kth.se>
2016-10-17 14:21:24 +0200

    testar att comitta hello

---
+++ hello.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 11:06:00 2016
+
+@author: Camilla
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    #Print "Hello World" and return None
+    print("later World :p")
+    
+def hello_two():
+    #Print "Hello World" and return None
+    print("Hello World 2 :p")
+
+# main program starts here
+hello()
+hello_two()
+
